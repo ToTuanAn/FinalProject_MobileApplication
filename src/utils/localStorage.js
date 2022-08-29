e606b18572e063a420f61b0f30b2f2fd41bdf28d
NguyenTrongTin <ntrongtin11702@gmail.com>
2022-08-29 14:48:24 +0700

    Store login session

---
+++ src/utils/localStorage.js
@@ -0,0 +1,30 @@
+import { AsyncStorage } from 'react-native';
+
+export const storeData = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(
+      `@LocalStore:${key}`,
+      value
+    );
+    console.log("Save data to local, ", value);
+  } catch (error) {
+      console.log("Save data from local failed");
+  }
+};
+
+export const retrieveData = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(
+      `@LocalStore:${key}`
+    );
+    if (value !== null) {
+      console.log("Get data from local, ", value);
+      return value
+    }
+  } catch (error) {
+      console.log("Get data from local failed");
+    throw "Error"
+  }
+};
+
+
